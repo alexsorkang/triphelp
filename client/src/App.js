314f8fcf40c25f018b713cb4e4f49569bac64565
alex sor kang <asork42@berkeley.edu>
2019-01-14 13:04:03 -0800

    more organized

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './Home'
-import NotFound from './NotFound'
+import Home from './components/home/Home'
+import NotFound from './components/not_found/NotFound'
+import Test from './components/test/Test'
 
 class App extends Component {
   render () {
     return <Router>
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/test' exact component={Test} />
         <Route component={NotFound} />
       </Switch>
     </Router>
