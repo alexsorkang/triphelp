ea2e5bd6ed17586efdd40dff3bea7947b2230c8b
alex sor kang <asork42@berkeley.edu>
2019-03-11 13:34:08 -0700

    edit mode button

--- app/controllers/api/itineraries_controller.rb
+++ app/controllers/api/itineraries_controller.rb
@@ -19,9 +19,13 @@ class Api::ItinerariesController < ApplicationController
     # query = 'Koreatown Los Angeles CA'
     query = params[:query]
     @client = GooglePlaces::Client.new(Rails.application.credentials.google[:secret_key])
-    # first = @client.spots_by_query(params, :types => ['restaurant'], :multipage => true)
     first = @client.spots_by_query(query, :types => ['restaurant'])
-    # search_names = first
+    first = JSON.parse(first.to_json)
+    google_keys = %w(name place_id id lng lat formatted_address rating address_components street_number street city region postal_code country)
+    first = first.map do |a|
+      a['id'] += Time.now.to_i.to_s
+      a.reject { |x| !google_keys.include?(x.to_s) }
+    end
     render json: first.to_json
     # second = @client.spots_by_pagetoken(first[-1].nextpagetoken)
   end
